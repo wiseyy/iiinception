02f10727fdd6cc5f9b0fd0b531c27792bbf16260
HemankBajaj <hemankbajaj05@gmail.com>
2022-05-06 16:02:11 +0530

    setted up the game loop

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "game.hpp"
 #include "constants.hpp"
 #include "Random.hpp"
-
+#include "Texture.hpp"
 using namespace std;
 
 int main() {
@@ -21,7 +21,22 @@ int main() {
 	}
 	else{
 		cout << "SDL initialized successfully\n";
+		Texture t1 = Texture();
+		bool x = t1.loadfromFile("map/b1.png", gRenderer);
+		cout<<"Media Loaded\n";
+		bool quit = false;
+		SDL_Event e;
+		while(!quit){
+			while(SDL_PollEvent(&e) != 0){
+				if( e.type == SDL_QUIT ){
+						quit = true;
+						cout << "Now Quitting....\n"; 
+				}
+			}
+			t1.setDimensions(20,20);
+			t1.render(0,0,gRenderer);
+			SDL_RenderPresent( gRenderer );
+		}
 	}
-    SDL_Init(SDL_INIT_VIDEO);
     return 0;
 }
