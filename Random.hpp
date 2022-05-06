3aeb6ac533146c73c82712483e6df77e9326789e
HemankBajaj <hemankbajaj05@gmail.com>
2022-05-06 15:41:52 +0530

    sdl initialized

---
+++ Random.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include<iostream>
+#include<vector>
+#include<time.h>
+
+using namespace std;
+
+// function to constraint a number in a given range
+int constraint(int x, int lower, int upper);
+// generate a random array of given dimension (elements might repeat)
+vector<int> generateRandomArray(int n, int lower, int upper);
+// generate a random array of given dimension (all elements are distinct)
+vector<int> generateRandomArrayDistinct(int n, int lower, int upper);
+// function to give a random integer every different time
+int getRandomInt(int lower, int upper);
+
+// implementation of the above design 
+int constraint(int x, int lower, int upper){
+	return x %(upper - lower + 1)  +  lower ;
+}
+
+vector<int> generateRandomVector(int n, int lower, int upper){
+	srand(time(0));
+	vector<int> randomVector(n);
+	for(int i = 0; i<n; ++i){
+		randomVector[i] = constraint(rand(), lower, upper);
+	}
+	return randomVector;
+}
+
+vector<int> generateRandomVectorDistinct(int n, int lower, int upper){
+	srand(time(0));
+	vector<int> randomVector(n);
+	int i = 0;
+	while(i<n){
+		int gen = constraint(rand(), lower, upper);
+		bool found = false;
+		for(int j = 0; j<i; ++j){
+			if (randomVector[j] == gen){
+				found = true; 
+				break; 
+			}
+		}
+		if (!found){
+			randomVector[i] = gen;
+			i++;
+		}
+	}
+	return randomVector;
+}
+
+int getRandomInt(int lower, int upper){
+	srand(time(0));
+	return constraint(rand(), lower, upper);
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include "SDL.h"
+#include "SDL_image.h"
+#include<vector>
+#include<utility>
+#include "game.hpp"
+#include "constants.hpp"
+#include "Random.hpp"
 
 using namespace std;
 
 int main() {
-    cout << "Hello World!\n";
+    cout << "Game Started";
+    cout<< "Screen Width : "<< SCREEN_WIDTH <<endl;
+	cout<< "Screen Height : "<< SCREEN_HEIGHT <<endl;
+	cout<< "MAP Width : "<< MAP_WIDTH <<endl;
+	cout<< "MAP Height : "<< MAP_HEIGHT <<endl;
+	cout<<"Total Tiles : "<< TOTAL_TILES<<endl;
+	if (!init()){
+		cout << "Could not initialize SDL\n";
+	}
+	else{
+		cout << "SDL initialized successfully\n";
+	}
     SDL_Init(SDL_INIT_VIDEO);
     return 0;
 }
